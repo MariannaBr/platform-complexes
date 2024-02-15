642cea0c49a5cfec42f9d5be9b6d622f47196953
MariannaBr <brezinova.marianna@gmail.com>
2024-02-15 15:39:10 -0800

    installed tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 
+// These styles apply to every route in the application
+import './globals.css'
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>

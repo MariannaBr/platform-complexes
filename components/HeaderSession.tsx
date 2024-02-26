aff5dd4aa23a6921f893edeaf30f05e30dfc51ef
MariannaBr <brezinova.marianna@gmail.com>
2024-02-26 13:52:53 -0800

    fixed HeaderSession

--- components/HeaderSession.tsx
+++ components/HeaderSession.tsx
@@ -41,7 +41,11 @@ const HeaderSession: React.FC<PropType> = ({
   if (!session) {
     right = (
       <div className="mt-4 flex md:ml-6 md:mt-0">
-        <Button title={titleFavorites} color="pink" link="/" />
+        <Button
+          title={titleFavorites}
+          buttonColor="button_colors_pink"
+          link="/"
+        />
         <Link href="/api/auth/signin">
           <a
             data-active={isActive("/signup")}
@@ -60,7 +64,11 @@ const HeaderSession: React.FC<PropType> = ({
         <p>
           {session.user.name} ({session.user.email})
         </p>
-        <Button title={titleFavorites} color="pink" link="/" />
+        <Button
+          title={titleFavorites}
+          buttonColor="button_colors_pink"
+          link="/"
+        />
         <button onClick={() => signOut()}>
           <a>Log out</a>
         </button>

8bb554908336861c7bfd255ee9dfdca688b8c461
MariannaBr <brezinova.marianna@gmail.com>
2024-02-27 11:35:34 -0800

    added GA script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 
 // These styles apply to every route in the application
 import "./globals.css";
@@ -12,6 +13,17 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
       <Head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-E65RTQKQZB"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-E65RTQKQZB');`}
+        </Script>
         <link
           href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.11.1/mapbox-gl.css"
           rel="stylesheet"

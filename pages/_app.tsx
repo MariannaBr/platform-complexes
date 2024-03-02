88c88b658978a33b0a2d1bb27d95083d12740795
MariannaBr <brezinova.marianna@gmail.com>
2024-03-01 16:04:40 -0800

    created _document for html links, adjusted _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
-import Head from "next/head";
 import Script from "next/script";
-import { metaTitle, metaDescription } from "../lib/defaults";
 
 // These styles apply to every route in the application
 import "./globals.css";
@@ -14,25 +12,6 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     // <SessionProvider session={pageProps.session}>
     <>
-      <Head>
-        <link
-          href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.11.1/mapbox-gl.css"
-          rel="stylesheet"
-        />
-        <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{metaTitle}</title>
-        <meta name="description" content={metaDescription} />
-        <meta name="robots" content="index, follow" />
-        <meta property="og:title" content={metaTitle} />
-        <meta property="og:description" content={metaDescription} />
-        <meta
-          property="og:image"
-          content="https://storage.googleapis.com/communitiesplatform-pictures/Winsdor/windsor_overview.webp"
-        />
-        <meta property="og:url" content="https://www.dogpatchapartments.com/" />
-        <meta property="og:type" content="website" />
-      </Head>
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-E65RTQKQZB"
---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+import { metaTitle, metaDescription } from "../lib/defaults";
+import Document, { Html, Main, Head, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.11.1/mapbox-gl.css"
+            rel="stylesheet"
+          />
+          <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>{metaTitle}</title>
+          <meta name="description" content={metaDescription} />
+          <meta name="robots" content="index, follow" />
+          <meta property="og:title" content={metaTitle} />
+          <meta property="og:description" content={metaDescription} />
+          <meta
+            property="og:image"
+            content="https://storage.googleapis.com/communitiesplatform-pictures/Winsdor/windsor_overview.webp"
+          />
+          <meta
+            property="og:url"
+            content="https://www.dogpatchapartments.com/"
+          />
+          <meta property="og:type" content="website" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

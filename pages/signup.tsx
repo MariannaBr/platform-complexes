030647c211a847bed26f23a022942ee655b9b3ca
MariannaBr <brezinova.marianna@gmail.com>
2024-03-13 13:46:20 +0100

    adjusted text on sign up page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -44,12 +44,11 @@ const SignupPage: React.FC = () => {
       <div className="bg-white -my-6 sm:my-24">
         <div className="mx-auto max-w-2xl xl:max-w-7xl">
           <div className="relative isolate overflow-hidden py-24 sm:rounded-3xl sm:px-24 xl:py-32">
-            <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-black sm:text-4xl">
-              Thank you for your interest.
+            <h2 className="mx-auto mb-8 max-w-2xl text-center text-3xl font-bold tracking-tight text-black sm:text-4xl">
+              We appreciate your interest. Thank you!
             </h2>
-            <p className="mx-2 sm:mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-800">
-              We are working on this feature. We will notify you as soon as we
-              are ready.
+            <p className="mx-2 sm:mx-auto mb-4 max-w-xl text-center text-lg leading-8 text-gray-800">
+              Register now, and we'll keep you updated with the latest news!
             </p>
 
             {feedbackMessage ? (
@@ -88,32 +87,6 @@ const SignupPage: React.FC = () => {
                 </button>
               </form>
             )}
-            {/* <svg
-              viewBox="0 0 1024 1024"
-              className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2"
-              aria-hidden="true"
-            >
-              <circle
-                cx={512}
-                cy={512}
-                r={512}
-                fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
-                fillOpacity="0.7"
-              />
-              <defs>
-                <radialGradient
-                  id="759c1415-0410-454c-8f7c-9a820de03641"
-                  cx={0}
-                  cy={0}
-                  r={1}
-                  gradientUnits="userSpaceOnUse"
-                  gradientTransform="translate(512 512) rotate(90) scale(512)"
-                >
-                  <stop stopColor="#db2777" />
-                  <stop offset={1} stopColor="#db2777" stopOpacity={0} />
-                </radialGradient>
-              </defs>
-            </svg> */}
           </div>
         </div>
       </div>

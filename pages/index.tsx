646181b5c3d311d291248b4b672a7521a7459071
MariannaBr <brezinova.marianna@gmail.com>
2024-03-26 14:48:55 +0100

    added scraping for next complexes

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,12 +17,20 @@ import {
   saveApartments,
   getApartmentsTenn,
   getApartmentsChase,
+  getApartmentsPotrero,
+  getApartmentsMartin,
+  getApartmentsMariposa,
+  getApartmentsGantry,
+  getApartmentsOAM,
 } from "../lib/apartmentsScrape";
 
 export const getStaticProps: GetStaticProps = async () => {
+  // run the scraping only once per day!!!
+
   //await saveApartments();
-  //await getApartmentsTenn();
-  // await getApartmentsChase("https://www.chasesf.com/availability");
+  // await getApartmentsOAM(
+  //   "https://www.oandmsf.com/apartments/ca/san-francisco/floor-plans#/"
+  // );
 
   const feed = await prisma.complex.findMany({
     where: {
